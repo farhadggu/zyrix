d1d8a71f4baac4531d106f5c3f8444f87eda38e4
Farhad Gharagozlu <79074346+farhadggu@users.noreply.github.com>
2025-06-25 10:15:47 +0330

    style: Update AnimateButton component to use rounded-2xl and lg:rounded-4xl for improved aesthetics

--- src/components/Shared/AnimateButton.tsx
+++ src/components/Shared/AnimateButton.tsx
@@ -4,7 +4,7 @@ import Button from "./Button";
 const AnimateButton = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="relative w-fit">
-      <div className="w-64 animated-conic-border mt-16 p-1 rounded-full border border-primary">
+      <div className="w-64 animated-conic-border mt-16 p-1 rounded-2xl lg:rounded-4xl border border-primary">
         <Button className="z-10 relative w-full font-semibold">{children}</Button>
       </div>
 
